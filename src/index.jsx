b35ed0733e2a99aed180ca10dcdec80b5d594fe0
Kika <kris.opletalova@gmail.com>
2023-04-16 15:09:42 +0200

    rozsekani na komponenty

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './style.css';
 
 const App = () => {
-  return (
-    <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
-    </div>
-  );
+    return (
+        <div className="container">
+            <nav className="menu">
+                <div className="menu-item">Home</div>
+                <div className="menu-item">Reviews</div>
+                <div className="menu-item">Articles</div>
+                <div className="menu-item">Movies</div>
+            </nav>
+
+            <div className="home">
+                <h1>Movie magazine</h1>
+                <p>Welcome to our website about movies! Here you will find the latest news from the world of cinema,
+                    movie reviews, interviews with actors and directors, and much more. Our pages are intended for all
+                    movie enthusiasts who are looking for information on the latest films, or who want to learn more
+                    about classic movies and their creators.</p>
+                <p>On our website, you will find reviews and ratings of the latest films that have just appeared in
+                    cinemas, as well as classic movies that have won audiences over for decades. Our editors strive to
+                    provide the most objective evaluations to help you decide which film deserves your attention. You
+                    can also look forward to exclusive interviews with directors, actors, and other creators who will
+                    reveal the secrets of filming and creating unforgettable movie scenes.</p>
+            </div>
+        </div>
+    );
 };
 
 createRoot(
-  document.querySelector('#app'),
-).render(<App />);
+    document.querySelector('#app'),
+).render(<App/>);
---
+++ src/pages/Movies/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Movies = () => {
+    return (
+        <div className="movies">
+            <h1>Movies</h1>
+
+            <div className="movies-container">
+
+                <nav className="movie-list">
+                    <a href="#" className="active">Pulp Fiction</a>
+                    <a href="#">1917</a>
+                    <a href="#">La La Land</a>
+                    <a href="#">Inception</a>
+                    <a href="#">Guardians of the Galaxy</a>
+                </nav>
+
+                <div className="movie">
+                    <img
+                        src="https://github.com/Czechitas-podklady-WEB/projekt-filmovy-magazin/raw/main/html-vzor/images/pulp-fiction.jpg"
+                        alt="Pulp Fiction" />
+
+                        <h2>Pulp Fiction</h2>
+
+                        <p>After stealing a mysterious orb in the far reaches of outer space, Peter Quill from Earth is
+                            now the main target of a manhunt led by the villain known as Ronan the Accuser. To help
+                            fight Ronan and his team and save the galaxy from his power, Quill creates a team of space
+                            heroes known as the "Guardians of the Galaxy" to save the galaxy.</p>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Movies;
