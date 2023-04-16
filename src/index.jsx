608f744ba5d1cb02e4eda4c68a77231948bc2d5a
Kika <kris.opletalova@gmail.com>
2023-04-16 15:23:25 +0200

    react router + nadefinovani cest

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,43 @@ import {createRoot} from 'react-dom/client';
 import './style.css';
 import Header from "./pages/Header";
 import Home from "./pages/Home";
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import Reviews from "./pages/Reviews";
+import Articles from "./pages/Articles";
+import Movies from "./pages/Movies";
 
 const App = () => {
     return (
         <div className="container">
             <Header/>
-            <Home/>
+            <Outlet/>
         </div>
     );
 };
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [{
+                path: "/",
+                element: <Home/>
+            },{
+                path: "/reviews",
+                element: <Reviews/>
+            },
+            {
+                path: "/articles",
+                element: <Articles/>
+            },
+            {
+                path: "/movies",
+                element: <Movies/>
+            }
+        ]
+    }
+]);
+
 createRoot(
     document.querySelector('#app'),
-).render(<App/>);
+).render(<RouterProvider router={router}/>);
