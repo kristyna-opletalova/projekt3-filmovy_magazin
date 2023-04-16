1133f6db516524a3eafd74702f55a68ce2224e03
Kika <kris.opletalova@gmail.com>
2023-04-16 15:15:57 +0200

    rozsekani na komponenty - part 2

---
+++ src/pages/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <nav className="menu">
+            <div className="menu-item">Home</div>
+            <div className="menu-item">Reviews</div>
+            <div className="menu-item">Articles</div>
+            <div className="menu-item">Movies</div>
+        </nav>
+    )
+}
+
+export default Header;
